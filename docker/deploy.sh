06ba8b3756b176650acd339cd150421f007ad6ad
User Docker <pegrobe+docker-user@gmail.com>
2025-06-29 02:34:51 +0200

    deploy.sh changed

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,6 +2,7 @@ cd dc_rest_api;
 docker build --no-cache -t docker.leibniz-lib.de:5000/dc_rest_api:latest .; docker push docker.leibniz-lib.de:5000/dc_rest_api:latest;
 cd ..;
 docker stack rm dc_rest_api;
+export $(cat compose.env);
 docker stack deploy --with-registry-auth -c docker-compose-swarm.yml dc_rest_api;
 
 
