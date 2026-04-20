121328f75054eda115b02eb1f7b7eb3edb6f7722
Björn Quast <bquast@q-terra.de>
2026-04-20 13:49:39 +0200

    set threading.daemon = True in asyncfunc

--- Queues/asyncfunc.py
+++ Queues/asyncfunc.py
@@ -6,5 +6,8 @@ from threading import Thread
 def asyncfunc(f):
 	def wrapper(*args, **kwargs):
 		thr = Thread(target=f, args=args, kwargs=kwargs)
+		# important to have it as a daemon here, so the thread will be killed when the parent thread is killed by ctrl c
+		# or when the tests.py ends
+		thr.daemon = True
 		thr.start()
 	return wrapper
