61e324eaf96772d046e16f338efb828420e18870
Björn Quast <bquast@q-terra.de>
2026-05-20 11:51:14 +0200

    daemon = True for asyncfunc in decorators

--- dc_rest_api/lib/decorators.py
+++ dc_rest_api/lib/decorators.py
@@ -4,7 +4,10 @@ http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xi-email-suppor
 from threading import Thread
 
 def asyncfunc(f):
-    def wrapper(*args, **kwargs):
-        thr = Thread(target=f, args=args, kwargs=kwargs)
-        thr.start()
-    return wrapper
+	def wrapper(*args, **kwargs):
+		thr = Thread(target=f, args=args, kwargs=kwargs)
+		# important to have it as a daemon here, so the thread will be killed when the parent thread is killed by ctrl c
+		# or when the tests.py ends
+		thr.daemon = True
+		thr.start()
+	return wrapper
