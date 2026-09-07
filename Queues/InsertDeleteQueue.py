e3212e596ac3c3021e95462b2689c1af407d6da8
Björn Quast <bquast@q-terra.de>
2026-09-07 14:50:45 +0200

    removed pudb breakpoints

--- Queues/InsertDeleteQueue.py
+++ Queues/InsertDeleteQueue.py
@@ -89,7 +89,6 @@ class InsertDeleteQueue(persistqueue.SQLiteQueue):
 
 	######### Implementation of tasks to start by the queue
 	def delete_DC_data(self, dc_params, request_params, task_id):
-		pudb.set_trace()
 		try:
 			dc_db = MSSQLConnector(config = dc_params)
 			
@@ -156,7 +155,6 @@ class InsertDeleteQueue(persistqueue.SQLiteQueue):
 	
 	
 	def insert_DC_data(self, dc_params, request_params, task_id):
-		pudb.set_trace()
 		try:
 			# DC connection must be set here, to prevent that it is ouddated when the task starts
 			dc_db = MSSQLConnector(config = dc_params)
